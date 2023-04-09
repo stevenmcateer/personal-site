f897b195551de90d0c1eb72a7840295aadf568ea
stevenmcateer <smcateer@wpi.edu>
2023-04-09 13:56:35 -0400

    remove phone number

--- src/Contact.js
+++ src/Contact.js
@@ -9,7 +9,7 @@ export default class Contact extends React.Component {
         return (
             <Container id={"contactDiv"} className="element">
                 <h2 align="center" className={"white sub-heading"}>CONTACT</h2>
-                <Grid stackable columns={4}>
+                <Grid stackable columns={3}>
                     <Grid.Column>
                         <a className={"white sub-heading"} href={"https://github.com/stevenmcateer"}>
                                 <Icon color={"white"} name='github' size='large' />
@@ -25,15 +25,15 @@ export default class Contact extends React.Component {
                     <Grid.Column>
                         <a className={"white sub-heading"} href={"mailto:smcateer@wpi.edu"}>
                             <Icon color={"white"} name='mail' size='large' />
-                            smcateer@wpi.edu
+                            stevenmcateer7@gmail.com
                         </a>
                     </Grid.Column>
-                    <Grid.Column>
+                    {/* <Grid.Column>
                         <a className={"white sub-heading"}>
                             <Icon color={"white"} name='phone' size='large' />
                             732-856-1395
                         </a>
-                    </Grid.Column>
+                    </Grid.Column> */}
                 </Grid>
 
             </Container>
